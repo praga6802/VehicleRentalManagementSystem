7d8beec7a320c18b35889a5daa576bd57e4ca256
Pragadeeswaran Sekar <124045929+praga6802@users.noreply.github.com>
2025-04-04 23:17:59 +0530

    Update RentalRules.java

--- RentalRules.java
+++ RentalRules.java
@@ -2,11 +2,15 @@ package rental;
 
 public interface RentalRules {
 
-    int MAX_NO_OF_DAYS=10;
-    String rentalName="JONI rentals";
-    String vehicleBrand="", vehicleModel="";
-    double calculateRentalCost();
-    boolean checkAvailability(String bikeBrand, String bikeModel);
-    void printReceipt();
-    void rentVehicleType();
+	int MAX_NO_OF_DAYS = 10;
+	String rentalName = "EZ Rent";
+	String vehicleBrand = "", vehicleModel = "";
+
+	double calculateRentalCost();
+
+	boolean checkAvailability(String bikeBrand, String bikeModel);
+
+	void printReceipt();
+
+	void rentVehicleType();
 }
