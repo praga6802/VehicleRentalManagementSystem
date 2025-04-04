f96df834c8a32ba65f716bbeb0e9972da2fc649f
Pragadeeswaran Sekar <124045929+praga6802@users.noreply.github.com>
2025-04-04 23:14:13 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,72 +1,105 @@
 package rental;
 
-public abstract class Vehicle{
+public abstract class Vehicle extends RentalAdapter {
 
-    protected String vehicleCategory;
-    protected String fuelType;
-    protected String vehicleType;
-    protected String vehicleBrand;
-    protected String vehicleModel;
-    protected int noOfDays;
-    protected String[] brandVehicles1; // Stores brand names
-    protected String[][] modelVehicles;
+	protected String vehicleCategory;
+	protected String fuelType;
+	protected String vehicleType;
+	protected String vehicleBrand;
+	protected String vehicleModel;
+	protected int noOfDays;
+	protected String[] brandVehiclesList; // Stores brand names
+	protected String[][] modelVehiclesList;
+	Customer customer;
 
-    Customer customer;
+	// for renting vehicles
+	public Vehicle(String vehicleType, Customer customer) {
+		this.vehicleType = vehicleType;
+		this.customer = new Customer(customer.getIdproof(), customer.getCustomer_name(), customer.getContactNo(),
+				customer.getAge(), customer.getNoOfDays(), customer.getLicenseAvl(), customer.getLicenseavlType(),
+				customer.getRentVehicleType());
+	}
 
-    public Vehicle(String vehicleCategory, String vehicleBrand, String vehicleModel, String fuelType, String vehicleType){
-        this.vehicleCategory=vehicleCategory;
-        this.vehicleBrand=vehicleBrand;
-        this.vehicleModel=vehicleModel;
-        this.fuelType=fuelType;
-        this.vehicleType=vehicleType;
-    }
-    //for renting
-    public Vehicle(String vehicleType, Customer customer){
-        this.vehicleType=vehicleType;
-        this.noOfDays= customer.getNoOfDays();
-        this.customer= new Customer(customer.getIdproof(), customer.getCustomer_name(),customer.getContactNo(),customer.getAge(),customer.getNoOfDays(), customer.getLicenseAvl(), customer.getLicenseavlType(),customer.getRentVehicleType());
+	public Vehicle(String vehicleType) {
+		this.vehicleType=vehicleType;
+	}
 
-    }
+	// for checking vehicle availability for bike
+	public Vehicle(String vehicleType, String[] brandVehiclesList, String[][] modelVehiclesList) {
+		this.vehicleType = vehicleType;
+		this.brandVehiclesList = brandVehiclesList;
+		this.modelVehiclesList = modelVehiclesList;
+	}
 
-    public Vehicle(){}
-    //for checking vehicle availability for bike
-    public Vehicle(String vehicleType,String[] brandVehicles1, String[][] modelVehicles){
-        this.vehicleType=vehicleType;
-        this.brandVehicles1=brandVehicles1;
-        this.modelVehicles=modelVehicles;
-    }
-    public Vehicle(String vehicleType, String vehicleBrand, String vehicleModel){
-        this.vehicleType=vehicleType;
-        this.vehicleBrand=vehicleBrand;
-        this.vehicleModel=vehicleModel;
-    }
+	public Vehicle(String vehicleType, String vehicleBrand, String vehicleModel) {
+		this.vehicleType = vehicleType;
+		this.vehicleBrand = vehicleBrand;
+		this.vehicleModel = vehicleModel;
+	}
 
+	// setters
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
 
-    // setters
-    public void setVehicleType(String vehicleType){
-        this.vehicleType=vehicleType;
-    }
-    public void setVehicleBrandList(String[] brandVehicles1){
-        this.brandVehicles1=brandVehicles1;
-    }
-    public void setVehicleModelList(String[][] modelVehicles){
-        this.modelVehicles=modelVehicles;
-    }
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
 
-    //getters
-    public String getVehicleType(){
-        return vehicleType;
-    }
-    public String[] getVehicleBrandList(){
-        return brandVehicles1;
-    }
-    public String[][] getVehicleModelList(){
-        return modelVehicles;
-    }
+	public void setVehicleCategory(String vehicleCategory) {
+		this.vehicleCategory = vehicleCategory;
+	}
 
-    public abstract boolean checkAvailability(String vehicleBrand, String vehicleModel);
-    public abstract void rentVehicleType();
-    public abstract void printReceipt();
-    public abstract double calculateRentalCost();
+	public void setVehicleBrand(String vehicleBrand) {
+		this.vehicleBrand = vehicleBrand;
+	}
+
+	public void setvehicleModel(String vehicleModel) {
+		this.vehicleModel = vehicleModel;
+	}
+
+	public void setNoOfDays(int noOfDays) {
+		this.noOfDays = noOfDays;
+	}
+
+	public void setVehicleBrandList(String[] brandVehiclesList) {
+		this.brandVehiclesList = brandVehiclesList;
+	}
+
+	public void setVehicleModelList(String[][] modelVehiclesList) {
+		this.modelVehiclesList = modelVehiclesList;
+	}
+
+	// getters
+	public String getVehicleType() {
+		return vehicleType;
+	}
+
+	public String getFuelType() {
+		return fuelType;
+	}
+
+	public String getVehicleCategory() {
+		return vehicleCategory;
+	}
+
+	public String getVehicleBrand() {
+		return vehicleBrand;
+	}
+
+	public String getVehicleModel() {
+		return vehicleModel;
+	}
+
+	public int getNoOfDays() {
+		return noOfDays;
+	}
+
+	public String[] getVehicleBrandList() {
+		return brandVehiclesList;
+	}
+
+	public String[][] getVehicleModelList() {
+		return modelVehiclesList;
+	}
 }
-
