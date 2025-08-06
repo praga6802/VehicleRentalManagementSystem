54d812f921c4e6b21541e5f10f057df876a49ebb
Pragadeeswaran Sekar <124045929+praga6802@users.noreply.github.com>
2025-08-07 00:30:31 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,8 +1,10 @@
 package rental;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public abstract class Vehicle implements RentalRules {
 
@@ -11,13 +13,16 @@ public abstract class Vehicle implements RentalRules {
 	private String vehicleSubType;
 	private String vehicleBrand;
 	private String vehicleModel;
-	private String[] brandVehiclesList; // Stores brand names
-	private String[][] modelVehiclesList;
+	private String[] vehicleBrandList; // Stores brand names
+	private String[][] vehicleModelList;
 	private String fuelType;
 	private double hourRent;
 	private double dayRent;
 	Customer customer;
 	String fuelTypeOption, vehicleTypeOption, brandTypeOption, modelTypeOption;
+	LocalDateTime date= LocalDateTime.now();
+	DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd-MM-yy hh:mm:ss a");
+	String dateFormat=date.format(formatter);
 
 
 	public Vehicle(String vehicleType, Customer customer) {
@@ -78,11 +83,11 @@ public abstract class Vehicle implements RentalRules {
 	public void setvehicleModel(String vehicleModel) {
 		this.vehicleModel = vehicleModel;
 	}
-	public void setVehicleBrandList(String[] brandVehiclesList) {
-		this.brandVehiclesList = brandVehiclesList;
+	public void setVehicleBrandList(String[] vehicleBrandList) {
+		this.vehicleBrandList = vehicleBrandList;
 	}
-	public void setVehicleModelList(String[][] modelVehiclesList) {
-		this.modelVehiclesList = modelVehiclesList;
+	public void setVehicleModelList(String[][] vehicleModelList) {
+		this.vehicleModelList = vehicleModelList;
 	}
 
 	// getters
@@ -99,15 +104,11 @@ public abstract class Vehicle implements RentalRules {
 		return vehicleModel;
 	}
 	public String[] getVehicleBrandList() {
-		return brandVehiclesList;
+		return vehicleBrandList;
 	}
 	public String[][] getVehicleModelList() {
-		return modelVehiclesList;
+		return vehicleModelList;
 	}
-
-
-
-	public abstract List<String> getRentVehicleDetails(String vehicleType);
 
 	public void receiptVerification(){
 		System.out.println("Do you want to your receipt?(Yes/No)");
@@ -119,8 +120,73 @@ public abstract class Vehicle implements RentalRules {
 		}
 	}
 
+
+	public boolean brandExists(List<String> brands, String bikeBrand) {
+		for (String brand : brands) {
+			if (brand.equalsIgnoreCase(bikeBrand))
+				return true;
+		}
+		return false;
+	}
+
+	public boolean modelExists(List<String> models, String bikeModel) {
+		for (String model : models) {
+			if (model.equalsIgnoreCase(bikeModel)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void chooseVehicleModel(String brandName, String brandModels[]) {
+		System.out.println("--- " + brandName + " " + getVehicleSubType() + " Models ---");
+		System.out.println("Select the Model from " + brandName);
+		for (int i = 0; i < brandModels.length; i++) {
+			System.out.println("Press " + (i + 1) + "-->" + brandName + " " + brandModels[i]);
+		}
+		int modelOption = sc.nextInt();
+		try {
+			if (modelOption >= 1 && modelOption <= brandModels.length) {
+				setVehicleModel(brandName + " " + brandModels[modelOption - 1]);
+			} else {
+				System.out.println("Invalid model option for" + brandName);
+			}
+		} catch (NumberFormatException n) {
+			System.out.println("Please enter a valid Number");
+		}
+	}
+
+	public double calculateKeyRent(Map<String, Integer> dayRates, Map<String,Integer> hourRate){
+		double rent=0.0;
+		String key = getFuelType().trim() + "-" + getVehicleSubType().trim() + "-" + getVehicleBrand().trim().toUpperCase();
+
+		if (customer.getNoOfDays() > 0) {
+			if (dayRates.containsKey(key)) {
+				rent = customer.getNoOfDays() * dayRates.get(key);
+				return rent;
+			} else {
+				System.out.println("No daily day rent available for selected vehicle");
+				return 0.0;
+			}
+		} else {
+			if (hourRate.containsKey(key)) {
+				rent = customer.getNoOfHours() * hourRate.get(key);
+				return rent;
+			} else {
+				System.out.println("No hourly rent available for selected vehicle");
+				return 0.0;
+			}
+		}
+	}
+
+
+	public abstract double calculateRentalCost();
+
+	public abstract List<String> getRentVehicleDetails(String type);
 	public void printReceipt() {
 		System.out.println("-------------Welcome to " + COMPANY_NAME + "----------------");
+		System.out.println("Date & Time:"+dateFormat);
+		System.out.println("=================================================");
 		System.out.println("Customer Name: " + customer.getCustomerName());
 		System.out.println("ID Proof: " + customer.getIdProof());
 		System.out.println("Age: " + customer.getAge());
