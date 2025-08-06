c3d09ac4075e3db079f83fbe9a339852677edd40
Pragadeeswaran Sekar <124045929+praga6802@users.noreply.github.com>
2025-08-06 13:51:35 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,106 +1,145 @@
 package rental;
 
-public abstract class Vehicle extends RentalAdapter {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-	protected String vehicleCategory;
-	protected String fuelType;
-	protected String vehicleType;
-	protected String vehicleBrand;
-	protected String vehicleModel;
-	protected int noOfDays;
-	protected String[] brandVehiclesList; // Stores brand names
-	protected String[][] modelVehiclesList;
+public abstract class Vehicle implements RentalRules {
+
+	Scanner sc= new Scanner(System.in);
+	private String vehicleType;
+	private String vehicleSubType;
+	private String vehicleBrand;
+	private String vehicleModel;
+	private String[] brandVehiclesList; // Stores brand names
+	private String[][] modelVehiclesList;
+	private String fuelType;
+	private double hourRent;
+	private double dayRent;
 	Customer customer;
+	String fuelTypeOption, vehicleTypeOption, brandTypeOption, modelTypeOption;
 
-	//Constructors
-	// for renting vehicles
+
 	public Vehicle(String vehicleType, Customer customer) {
+
 		this.vehicleType = vehicleType;
-		this.customer = new Customer(customer.getIdproof(), customer.getCustomerName(), customer.getContactNo(),
-				customer.getAge(), customer.getNoOfDays(), customer.getLicenseAvl(), customer.getLicenseavlType(),
-				customer.getRentVehicleType());
+		//this.customer=customer;
+		this.customer = new Customer(customer.getIdProof(), customer.getCustomerName(), customer.getContactNo(),customer.getAge(), customer.getNoOfDays(),customer.getNoOfHours(), customer.getLicenseAvl(), customer.getLicenseAvlType(),customer.getRentVehicleType());
 	}
-
-	public Vehicle(String vehicleType) {
-		this.vehicleType=vehicleType;
-	}
-
-	// for checking vehicle availability for bike
-	public Vehicle(String vehicleType, String[] brandVehiclesList, String[][] modelVehiclesList) {
-		this.vehicleType = vehicleType;
-		this.brandVehiclesList = brandVehiclesList;
-		this.modelVehiclesList = modelVehiclesList;
-	}
-
 	public Vehicle(String vehicleType, String vehicleBrand, String vehicleModel) {
 		this.vehicleType = vehicleType;
 		this.vehicleBrand = vehicleBrand;
 		this.vehicleModel = vehicleModel;
 	}
 
-	// setters()
-	public void setVehicleType(String vehicleType) {
-		this.vehicleType = vehicleType;
+	public Vehicle(String vehicleType){
+		this.vehicleType=vehicleType;
 	}
 
-	public void setFuelType(String fuelType) {
-		this.fuelType = fuelType;
-	}
-
-	public void setVehicleCategory(String vehicleCategory) {
-		this.vehicleCategory = vehicleCategory;
-	}
-
-	public void setVehicleBrand(String vehicleBrand) {
-		this.vehicleBrand = vehicleBrand;
-	}
-
-	public void setvehicleModel(String vehicleModel) {
+	public void setVehicleModel(String vehicleModel) {
 		this.vehicleModel = vehicleModel;
 	}
 
-	public void setNoOfDays(int noOfDays) {
-		this.noOfDays = noOfDays;
+	public double getHourRent() {
+		return hourRent;
 	}
 
+	public void setHourRent(double hourRent) {
+		this.hourRent = hourRent;
+	}
+
+	public double getDayRent() {
+		return dayRent;
+	}
+
+	public void setDayRent(double dayRent) {
+		dayRent = dayRent;
+	}
+
+
+	public String getVehicleSubType() {
+		return vehicleSubType;
+	}
+
+	public void setVehicleSubType(String vehicleSubType) {
+		this.vehicleSubType = vehicleSubType;
+	}
+
+
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+	public void setVehicleBrand(String vehicleBrand) {
+		this.vehicleBrand = vehicleBrand;
+	}
+	public void setvehicleModel(String vehicleModel) {
+		this.vehicleModel = vehicleModel;
+	}
 	public void setVehicleBrandList(String[] brandVehiclesList) {
 		this.brandVehiclesList = brandVehiclesList;
 	}
-
 	public void setVehicleModelList(String[][] modelVehiclesList) {
 		this.modelVehiclesList = modelVehiclesList;
 	}
 
-	// getters()
+	// getters
 	public String getVehicleType() {
 		return vehicleType;
 	}
-
 	public String getFuelType() {
 		return fuelType;
 	}
-
-	public String getVehicleCategory() {
-		return vehicleCategory;
-	}
-
 	public String getVehicleBrand() {
 		return vehicleBrand;
 	}
-
 	public String getVehicleModel() {
 		return vehicleModel;
 	}
-
-	public int getNoOfDays() {
-		return noOfDays;
-	}
-
 	public String[] getVehicleBrandList() {
 		return brandVehiclesList;
 	}
-
 	public String[][] getVehicleModelList() {
 		return modelVehiclesList;
+	}
+
+
+
+	public abstract List<String> getRentVehicleDetails(String vehicleType);
+
+	public void receiptVerification(){
+		System.out.println("Do you want to your receipt?(Yes/No)");
+		String receipt = sc.next();
+		if (receipt.equalsIgnoreCase("yes")) {
+			printReceipt();
+		} else {
+			System.out.println("Thank you for keeping green environment");
+		}
+	}
+
+	public void printReceipt() {
+		System.out.println("-------------Welcome to " + COMPANY_NAME + "----------------");
+		System.out.println("Customer Name: " + customer.getCustomerName());
+		System.out.println("ID Proof: " + customer.getIdProof());
+		System.out.println("Age: " + customer.getAge());
+		System.out.println("Contact Number: " + customer.getContactNo());
+		System.out.println("License Available Type: " + customer.getLicenseAvlType());
+		System.out.println("Rent Vehicle: " + getVehicleType());
+		System.out.println("Fuel Type: " + getFuelType());
+		System.out.println("Vehicle Type: " + getVehicleSubType());
+		System.out.println("Vehicle Brand: " + getVehicleBrand());
+		System.out.println("Vehicle Model: " + getVehicleModel());
+		double rentalCost=calculateRentalCost();
+		if (customer.getNoOfDays() > 0) {
+			System.out.println("No Of Days: " + customer.getNoOfDays());
+		} else {
+			System.out.println("Hours Taken: " + customer.getNoOfHours());
+		}
+		System.out.println("Rental Cost= " + rentalCost);
+		System.out.println("==========================================");
+		System.out.println("THANK YOU! VISIT AGAIN!");
+		sc.close();
 	}
 }
