e54b4e94622a643d9721dfab0968cc4bc7d41395
Pragadeeswaran Sekar <124045929+praga6802@users.noreply.github.com>
2025-08-07 00:33:41 +0530

    Update VehicleRentMain.java

--- VehicleRentMain.java
+++ VehicleRentMain.java
@@ -8,7 +8,7 @@ import static rental.RentalRules.*;
 public class VehicleRentMain{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Vehicle vehicle;
+		Vehicle vehicle = null;
 		System.out.println("Welcome to "+ COMPANY_NAME+"..");
 		System.out.println("Press 1 --> Check Vehicle Available, Press 2 --> Rent Vehicle, Press 3 --> Exit");
 		String doOperation = sc.next();
@@ -24,29 +24,29 @@ public class VehicleRentMain{
 			switch (vehicleType) {
 			
 			// check vehicles available
-			case "1": // bike
-				vehicleType = "Bike";
+			case "1": // bike details
 				vehicle=new Bike(vehicleType);
-				List<String> bikeDetails=vehicle.getRentVehicleDetails(vehicleType);
+				vehicle.setVehicleType("Bike");
+				List<String> bikeDetails=vehicle.getRentVehicleDetails(vehicle.getVehicleType());
 				String bikeBrand= bikeDetails.get(0);
 				String bikeModel=bikeDetails.get(1);
 				vehicle.checkAvailability(vehicleType,bikeBrand,bikeModel);
 				break;
 
 				
-			case "2": // car
-				vehicleType = "Car";
+			case "2": // car details
 				vehicle=new Car(vehicleType);
-				List<String> carDetails=vehicle.getRentVehicleDetails(vehicleType);
+				vehicle.setVehicleType("Car");
+				List<String> carDetails=vehicle.getRentVehicleDetails(vehicle.getVehicleType());
 				String carBrand= carDetails.get(0);
 				String carModel=carDetails.get(1);
 				vehicle.checkAvailability(vehicleType,carBrand,carModel);
 				break;
 				
-			case "3": // truck
-				vehicleType = "Truck";
+			case "3": // truck details
 				vehicle=new Truck(vehicleType);
-				List<String> truckDetails=vehicle.getRentVehicleDetails(vehicleType);
+				vehicle.setVehicleType("Truck");
+				List<String> truckDetails=vehicle.getRentVehicleDetails(vehicle.getVehicleType());
 				String truckBrand= truckDetails.get(0);
 				String truckModel=truckDetails.get(1);
 				vehicle.checkAvailability(vehicleType,truckBrand,truckModel);
@@ -56,98 +56,24 @@ public class VehicleRentMain{
 
 		// rent vehicles
 		case "2":
-			System.out.println("Enter Customer details...");
-			System.out.println("Enter Customer Name: ");
-			String customer_name = sc.nextLine();
-			System.out.println("Enter Age: ");
-			int age = sc.nextInt();
-			sc.nextLine();
-			if (age < 18) {
-				System.out.println("Your are below age 18.. We are not provide for rentals..");
-				sc.close();
-				return;
-			}
-			System.out.println("Do you have License(Yes/No)?");
-			String licenseAvl = sc.next();
-			String licenseAvlType = "";
-			if (licenseAvl.equalsIgnoreCase("yes")) {
-				System.out.println("Enter the license of Vehicle Type(Car/Bike/Truck)");
-				licenseAvlType = sc.next();
-			} else {
-				System.out.println("Without Driving License.. We would not provide vehicles for rentals");
-				sc.close();
-				return;
-			}
-			System.out.println("ID Proof acceptable for renting \n Press 1. Aadhar, Press 2. Voter ID");
-			String idProof = sc.next();
-			switch (idProof) {
-			case "1":
-				idProof = "Aadhar";
-				break;
-			case "2":
-				idProof = "Voter ID";
-				break;
-			default:
-				System.out.println("Without ID Proof we are not provide for rentals...");
-				return;
-			}
-			System.out.println("Enter Contact Number: ");
-			String contactNo = sc.next();
-			if (contactNo.length() != 10) {
-				System.out.println("Please provide 10 digit mobile number..");
-				sc.close();
-				return;
-			}
-
-			System.out.println("Enter the hours/days");
-			String durationChoice=sc.next();
-			double noOfDays = 0.0,noOfHours=0.0;
-			if(durationChoice.equalsIgnoreCase("days")) {
-				System.out.println("Enter the No of Days:");
-				 noOfDays= sc.nextDouble();
-				sc.nextLine();
-				if (noOfDays > MAX_NO_OF_DAYS) {
-					System.out.println("We are not provide more than 10 days...");
-					sc.close();
-					return;
-				}
-			}
-			else{
-				System.out.println("Enter the no of Hours:");
-				noOfHours=sc.nextDouble();
-				sc.nextLine();
-			}
-			String rentVehicleType = "";
-			// case for selecting normal or ev vehicles
-			String receipt = "";
-
-			Customer customer = new Customer(idProof, customer_name, contactNo, age, noOfDays,noOfHours, licenseAvl, licenseAvlType, rentVehicleType);
-
-			if (licenseAvlType.equalsIgnoreCase("Bike")) {
+			Customer customer=Customer.registerDetails();
+			String rentVehicleType = null;
+			if (customer.getRentVehicleType().equalsIgnoreCase("Bike")) {
 				System.out.println("You are eligible only for Bike Rentals..");
-				vehicle = new Bike(rentVehicleType, customer);
-				vehicle.setVehicleType("Bike");
-				vehicle.rentVehicleType();
-				vehicle.receiptVerification();
+				vehicle = new Bike("Bike", customer);
 			}
 			// customer has car license - bike, car
-			else if (licenseAvlType.equalsIgnoreCase("Car")) {
+			else if (customer.getRentVehicleType().equalsIgnoreCase("Car")) {
 				System.out.println("Available Vehicle Types");
 				System.out.println("Press 1 --> Bike, Press 2 --> Car");
 				System.out.println("Which type of Vehicle would you like to rental?");
 				rentVehicleType = sc.next();
 				switch (rentVehicleType) {
 				case "1":
-					vehicle = new Bike(rentVehicleType, customer);
-					vehicle.setVehicleType("Bike");
-					vehicle.rentVehicleType();
-					vehicle.receiptVerification();
+					vehicle = new Bike("Bike", customer);
 					break;
 				case "2":
-					vehicle = new Car(rentVehicleType, customer);
-					vehicle.setVehicleType("Car");
-					vehicle.rentVehicleType();
-					vehicle.receiptVerification();
+					vehicle = new Car("Car", customer);
 					break;
 				default:
 					System.out.println("You did not select any option!");
@@ -155,35 +81,28 @@ public class VehicleRentMain{
 			}
 
 			// customer has truck license - bike, car and truck
-			else if (licenseAvlType.equalsIgnoreCase("truck")) {
+			else if (customer.getRentVehicleType().equalsIgnoreCase("Truck")) {
 				System.out.println("Available Vehicle Types");
 				System.out.println("Press 1 --> Bike, Press 2 --> Car, Press 3 --> Truck");
 				System.out.println("Which type of Vehicle would you like to rental?");
 				rentVehicleType = sc.next();
 				switch (rentVehicleType) {
 				case "1":
-					rentVehicleType = "Bike";
-					vehicle = new Bike(rentVehicleType, customer);
-					vehicle.rentVehicleType();
-					vehicle.receiptVerification();
+					vehicle = new Bike("Bike", customer);
 					break;
 				case "2":
-					rentVehicleType = "Car";
-					vehicle = new Car(rentVehicleType, customer);
-					vehicle.rentVehicleType();
-					vehicle.receiptVerification();
+					vehicle = new Car("Car", customer);
 					break;
 				case "3":
-					rentVehicleType = "Truck";
-					vehicle = new Truck(rentVehicleType, customer);
-					vehicle.rentVehicleType();
-					vehicle.receiptVerification();
+					vehicle = new Truck("Truck", customer);
 					break;
 				default:
 					System.out.println("You did not select any option!");
 				}
-			} else {
-				System.out.println("Please Enter Valid Vehicle Name");
+			}
+			if(vehicle!=null){
+				vehicle.rentVehicleType();
+				vehicle.receiptVerification();
 			}
 			break;
 
@@ -193,7 +112,6 @@ public class VehicleRentMain{
 			System.out.println("Thank you for your precious time!");
 			sc.close();
 			return;
-
 		// if the user enters wrong option
 		default:
 			System.out.println("Your option is Invalid!");
